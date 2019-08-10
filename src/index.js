bf3ac5e5df397b6375be8a71ac166826cbb27933
trainbolt <t.rainbolt@rogers.com>
2019-08-10 00:36:18 -0700

    Reupload of files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 
 ReactDOM.render(
   <App />,
-  document.getElementById("root") // eslint-disable-line no-undef
+  document.getElementById("root")
 );
-// eslint-disable-next-line no-undef
-if (module.hot) module.hot.accept(); // eslint-disable-line no-undef
+
